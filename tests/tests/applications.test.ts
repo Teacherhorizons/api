ca51a6682ddcd240f7f7638df5c8279678b2cd3e
Asma <saiyadasmaba@gmail.com>
2022-06-23 17:18:52 +0530

    filters for list of applications, tests

--- tests/tests/applications.test.ts
+++ tests/tests/applications.test.ts
@@ -15,6 +15,12 @@ test('GET applications/1?schema=admin', async () => {
 });
 
 test('GET applications?schema=admin&filter[id][contains]=1', async () => {
+  const response = await api.get('applications?schema=admin&filter[status.id][contains]=1&filter[job.school.name][matches]=alic');
+  expect(response.status).toEqual(200);
+  expect(response).toSatisfyApiSpec();
+});
+
+test('GET applications?schema=admin&filter[id][contains]=1', async () => {
   const response = await api.get('applications?schema=admin&filter[id][contains]=1');
   expect(response.status).toEqual(200);
   expect(response).toSatisfyApiSpec();
