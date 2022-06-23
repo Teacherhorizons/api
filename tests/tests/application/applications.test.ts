576171b63466ef0ed3e78c9c4123fe24997e6372
Ryan Randall <ryan@teacherhorizons.com>
2022-06-23 14:40:13 +0100

    new applications test

--- tests/tests/application/applications.test.ts
+++ tests/tests/application/applications.test.ts
@@ -7,3 +7,9 @@ test('GET applications?schema=admin&filter[id][contains]=1', async () => {
   expect(response.status).toEqual(200);
   expect(response).toSatisfyApiSpec();
 });
+
+test('GET applications?schema=admin&filter[id][contains]=1...', async () => {
+  const response = await api.get('applications?schema=admin&filter[status.id][contains]=1&filter[job.school.name][matches]=alic');
+  expect(response.status).toEqual(200);
+  expect(response).toSatisfyApiSpec();
+});
