0f6757d6a24dfd042723fa111e46d1c1db7adb5a
Ryan Randall <ryan@teacherhorizons.com>
2023-04-24 12:14:51 +0100

    integer -> boolean WIP

--- tests/interfaces/teacher/teacher.d.ts
+++ tests/interfaces/teacher/teacher.d.ts
@@ -8,7 +8,7 @@ declare namespace JsonApi {
       fullName?: string;
       thPlaceabilityRating?: number;
       memberNumber?: number;
-      thDoNotContact?: number; // TODO: switch to boolean
+      thDoNotContact?: boolean;
     };
   }
 }
