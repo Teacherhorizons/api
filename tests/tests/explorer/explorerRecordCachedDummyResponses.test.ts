e6b2750d80604bec41dec19709e7fae5f2a947b1
Ryan Randall <ryan@teacherhorizons.com>
2023-11-29 03:25:47 +0000

    explorer record dummy data tests

--- tests/tests/explorer/explorerRecordCachedDummyResponses.test.ts
+++ tests/tests/explorer/explorerRecordCachedDummyResponses.test.ts
@@ -1,36 +1,66 @@
 import * as shared from '../../shared';
 
-const includeTestNames: string[] = null;
-// const includeTestNames = ['explorer-records - teacher'];
+// const includeTestNames: string[] = null;
+const includeTestNames = ['explorer-records - admin 101', 'explorer-records - admin 102'];
 
 var tests = shared.addTestGroups(
   [],
   [
     {
-      getUrl: (data) => `explorer-records?schema=admin`,
+      getUrl: (data) => `explorer-records/101?schema=admin&include=activities,schoolUser,schoolUser.schools,teacher`,
       tests: [
         {
-          name: 'explorer-records - admin',
+          name: 'explorer-records - admin 101',
           userEmail: 'admin@th.test',
           expectedStatus: 200,
         },
       ],
     },
     {
-      getUrl: (data) => `explorer-records?schema=school`,
+      getUrl: (data) => `explorer-records/102?schema=admin&include=activities,schoolUser,schoolUser.schools,teacher`,
       tests: [
         {
-          name: 'explorer-records - school',
+          name: 'explorer-records - admin 102',
+          userEmail: 'admin@th.test',
+          expectedStatus: 200,
+        },
+      ],
+    },
+    {
+      getUrl: (data) => `explorer-records/101?schema=school&include=activities,teacher`,
+      tests: [
+        {
+          name: 'explorer-records - school 101',
           userEmail: 'school-1-school@th.test',
           expectedStatus: 200,
         },
       ],
     },
     {
-      getUrl: (data) => `explorer-records?schema=teacher`,
+      getUrl: (data) => `explorer-records/102?schema=school&include=activities,teacher`,
       tests: [
         {
-          name: 'explorer-records - teacher',
+          name: 'explorer-records - school 102',
+          userEmail: 'school-1-school@th.test',
+          expectedStatus: 200,
+        },
+      ],
+    },
+    {
+      getUrl: (data) => `explorer-records/101?schema=teacher&include=activities,schoolUser,schoolUser.schools`,
+      tests: [
+        {
+          name: 'explorer-records - teacher 101',
+          userEmail: 'endorsed@th.test',
+          expectedStatus: 200,
+        },
+      ],
+    },
+    {
+      getUrl: (data) => `explorer-records/102?schema=teacher&include=activities,schoolUser,schoolUser.schools`,
+      tests: [
+        {
+          name: 'explorer-records - teacher 102',
           userEmail: 'endorsed@th.test',
           expectedStatus: 200,
         },
@@ -42,7 +72,7 @@ var tests = shared.addTestGroups(
 tests = tests.sort(shared.compareFnGenerator(['userEmail']));
 tests = tests.filter((t) => includeTestNames == null || includeTestNames.includes(t.name));
 
-describe('get-dummy-explorer-records', () => {
+describe('get-dummy-explorer-record', () => {
   beforeAll(async () => {
     await shared.setupBeforeAll();
   });
@@ -60,8 +90,8 @@ describe('get-dummy-explorer-records', () => {
           expect(t.getPassesCustomChecks(response.data, shared.data)).toBe(true);
         }
 
-        // const isResponseValid = shared.getIsResponseValid(response.data);
-        // expect(isResponseValid).toBe(true);
+        const isResponseValid = shared.getIsResponseValid(response.data);
+        expect(isResponseValid).toBe(true);
 
         expect(response).toSatisfyApiSpec();
       } else {
