1dcf4ae32ea03ad1a82f44de334896c8d4032501
Jon-Peppinck <mrjonpeppinck@gmail.com>
2024-02-12 14:26:01 +0700

    explorer record RW - teacher payload tweaks

--- tests/tests/explorer/explorerRecordRW.test.ts
+++ tests/tests/explorer/explorerRecordRW.test.ts
@@ -2,33 +2,37 @@ import { deleteTestData, postAddedTestData } from '../../data/main';
 import * as shared from '../../shared';
 
 const includeTestNames: string[] = null;
+// const includeTestNames: string[] = ['explorer-records?asUserId=${data.users[1].id} - teacher id && memberNumber'];
 
 let explorerRecordIds: number[] = [];
+
+const hasTeacherId = true;
+const hasTeacherMemberNumber = true;
 
 let tests = shared.addTestGroups(
   [],
   [
     {
       getUrl: (data) => `explorer-records`,
-      getPayload: (data) => getExplorerRecordPayload(),
+      getPayload: (data) => getExplorerRecordPayload(hasTeacherId, !hasTeacherMemberNumber),
       tests: [
         {
-          name: 'explorer-records',
+          name: 'explorer-records - teacherId',
           userEmail: 'school-1-school@th.test',
           expectedStatus: 201,
         },
         {
-          name: 'explorer-records',
+          name: 'explorer-records - teacherId',
           userEmail: 'signedOut',
           expectedStatus: 401, // accessNotPermitted
         },
         {
-          name: 'explorer-records',
+          name: 'explorer-records - teacherId',
           userEmail: 'endorsed@th.test',
           expectedStatus: 401, // accessNotPermitted
         },
         {
-          name: 'explorer-records',
+          name: 'explorer-records - teacherId',
           userEmail: 'admin@th.test',
           expectedStatus: 404, // schoolUserId not found
         },
@@ -36,46 +40,75 @@ let tests = shared.addTestGroups(
     },
     {
       getUrl: (data) => `explorer-records?asUserId=${data.users[1].id}`,
-      getPayload: (data) => getExplorerRecordPayload(),
+      getPayload: (data) => getExplorerRecordPayload(!hasTeacherId, hasTeacherMemberNumber),
       tests: [
         {
-          name: 'explorer-records?asUserId=${data.users[1].id}',
+          name: 'explorer-records?asUserId=${data.users[1].id} - teacher memberNumber',
+          userEmail: 'admin@th.test',
+          expectedStatus: 201,
+        },
+        {
+          name: 'explorer-records?asUserId=${data.users[1].id} - teacher memberNumber',
           userEmail: 'school-1-school@th.test',
           expectedStatus: 401, // accessNotPermitted
         },
         {
-          name: 'explorer-records?asUserId=${data.users[1].id}',
+          name: 'explorer-records?asUserId=${data.users[1].id} - teacher memberNumber',
           userEmail: 'signedOut',
           expectedStatus: 401, // accessNotPermitted
         },
         {
-          name: 'explorer-records?asUserId=${data.users[1].id}',
+          name: 'explorer-records?asUserId=${data.users[1].id} - teacher memberNumber',
           userEmail: 'endorsed@th.test',
           expectedStatus: 401, // accessNotPermitted
         },
+      ],
+    },
+    {
+      getUrl: (data) => `explorer-records?asUserId=${data.users[1].id}`,
+      getPayload: (data) => getExplorerRecordPayload(hasTeacherId, hasTeacherMemberNumber),
+      tests: [
         {
-          name: 'explorer-records?asUserId=${data.users[1].id}',
+          name: 'explorer-records?asUserId=${data.users[1].id} - teacher id && memberNumber',
           userEmail: 'admin@th.test',
-          expectedStatus: 201,
+          expectedStatus: 400, // invalidJSONInput - Only one of teacherId or memberNumber, not both
+        },
+      ],
+    },
+    {
+      getUrl: (data) => `explorer-records?asUserId=${data.users[1].id}`,
+      getPayload: (data) => getExplorerRecordPayload(!hasTeacherId, !hasTeacherMemberNumber),
+      tests: [
+        {
+          name: 'explorer-records?asUserId=${data.users[1].id} - no teacher id or memberNumber',
+          userEmail: 'admin@th.test',
+          expectedStatus: 400, // missingMandatoryParameter - teacherId or memberNumber required
         },
       ],
     },
   ]
 );
 
-const getExplorerRecordPayload = () => {
-  return {
+const getExplorerRecordPayload = (hasTeacherId: boolean, hasMemberNumber: boolean) => {
+  const payload = {
     data: {
       type: 'explorer-record',
       attributes: {
         text: 'foo bar baz',
-        teacher: {
-          id: '2513',
-          memberNumber: '123724',
-        },
+        teacher: {} as { id?: string; memberNumber?: string },
       },
     },
   };
+
+  if (hasTeacherId) {
+    payload.data.attributes.teacher.id = '2513';
+  }
+
+  if (hasMemberNumber) {
+    payload.data.attributes.teacher.memberNumber = '123724';
+  }
+
+  return payload;
 };
 
 tests = tests.sort(shared.compareFnGenerator(['userEmail']));
